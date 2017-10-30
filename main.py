f3f23ab15193ba34804bd6b7efb6e5ac317a5044
Ilya Olegovich <fedotow.ilja@gmail.com>
2017-10-30 22:15:06 +0000

    Added some changes in main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#coding: utf-8
+
+#####
+####Загляните в техническое задание/руководство к работе
+#####
+
+from gui import *
+
+def main():          #Иницилизация функции main для работы главного файла
+    print("Hello, buys")
+    gui()
+
+if __name__ == "__main__":
+    main()
