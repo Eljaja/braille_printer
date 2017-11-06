9b8e0cfbe75052470ebf4596fb41e6a1a4d0ec3b
Ilya Olegovich <fedotow.ilja@gmail.com>
2017-11-06 20:30:07 +0000

    Сделан GUI и переделан алгоритм перевода

--- BrailleTranslator.py
+++ BrailleTranslator.py
@@ -1,38 +1,32 @@
 #!/usr/bin/python3
 #coding: utf-8
 
-class BrailleTranslator:
-    
-    def __init__(self,text):
-        self.text = text
-        self.sheet = ['','','',''] # исходный лист
-        self.string = 0 # номер строки
-        
-        
-    def en_translation(self):  
-        for symbol in self.text:  #посимвольная обработка
-            if symbol == 'a':
-                self.sheet[0+self.string*4] += '* '
-                self.sheet[1+self.string*4] += '  '
-                self.sheet[2+self.string*4] += '  '
-                self.sheet[3+self.string*4] += '  '
-                
-            if symbol == 'b':
-                self.sheet[0+self.string*4] += '* '
-                self.sheet[1+self.string*4] += '* '
-                self.sheet[2+self.string*4] += '  '
-                self.sheet[3+self.string*4] += '  '
-            
-            if symbol == 'c':
-                self.sheet[0+self.string*4] += '**'
-                self.sheet[1+self.string*4] += '  '
-                self.sheet[2+self.string*4] += '  '
-                self.sheet[3+self.string*4] += '  '
-                
-            if symbol == 'd':
-                self.sheet[0+self.string*4] += '**'
-                self.sheet[1+self.string*4] += ' *'
-                self.sheet[2+self.string*4] += '  '
-                self.sheet[3+self.string*4] += '  '
-        
-        return self.sheet
+from constant1 import alph
+class BrailleTranslation:
+    def __init__(self,id):
+        self.id = id # 0 - Arduino, 1 - GUI (Работает частично)
+        self.brailleList = []
+        self.isDigit = False
+
+
+
+    def translation(self,string):
+        for symbol in string:
+            if symbol.isupper():
+                self.brailleList.append(alph["UP"][self.id])
+                self.brailleList.append(alph[symbol.lower()][self.id])
+
+            elif symbol.isdigit():
+                if not self.isDigit:
+                    self.isDigit = True
+                    self.brailleList.append(alph["NUM"][self.id])
+                self.brailleList.append(alph[symbol][self.id])
+
+            else:
+                self.isDigit = False
+                self.brailleList.append(alph[symbol][self.id])
+
+        return self.brailleList
+
+test = BrailleTranslation(0)
+print(test.translation("12"))
