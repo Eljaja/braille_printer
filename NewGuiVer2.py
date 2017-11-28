04dc06c3bf788817ac2bebd57dbe73931b0a382e
Ilya_Olegovich <IlyaFedotov@masme.ru>
2017-11-28 13:58:24 +0300

    Сделана тестовая часть гуи, в будущем - чуть переделать рисовалку

---
+++ NewGuiVer2.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+from DrawList import DrawList
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+app = QApplication(sys.argv)
+
+
+class BrailleGui(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        #grid = QGridLayout()
+        #self.setLayout(grid)
+        #
+        self.textbox = QTextEdit(self)
+        self.textbox.resize(310,430)
+        self.textbox.move(10,10)
+
+
+        self.pic = QLabel(self)
+        self.pic.resize(310,430)
+        self.pic.move(330,10)
+
+        TranslateButton = QPushButton("Translate",self)
+        TranslateButton.move(10,445)
+        TranslateButton.resize(100,30)
+
+        LoadFromFileButton = QPushButton("From File..",self)
+        LoadFromFileButton.move(120,445)
+        LoadFromFileButton.resize(100,30)
+        PrintButton = QPushButton("Print!",self)
+
+        PrintButton.move(230,445)
+        PrintButton.resize(100,30)
+
+        TranslateButton.clicked.connect(self.eventTranslate)
+        LoadFromFileButton.clicked.connect(self.showDialog)
+        PrintButton.clicked.connect(self.eventPrint)
+
+        self.setGeometry(300,100,640,500)
+
+        self.setGeometry(300,100,640,500)
+        self.setWindowTitle("Printer V0.1")
+        self.setWindowIcon(QIcon('braille.png'))
+        self.show()
+
+    def eventPrint(self): #Я ещё тут подумаю
+        pass
+
+    def eventTranslate(self):
+
+        Draw = DrawList()
+        text = Draw.drawBraille(str(self.textbox.toPlainText()))
+        Draw.saveFile()
+        self.pic.setPixmap(QPixmap("""./picture.png"""))
+        self.pic.resize(310,430)
+        self.pic.show()
+
+    def showDialog(self):
+        try:
+            fname = QFileDialog.getOpenFileNames(self, "Open file")[0][0]
+        except IndexError:
+            pass
+        #Конструкция try-except сделана для того, чтобы пофиксить баг с вылетом
+        #из-за нажатия крестика в меню выбора файла
+
+        with open(fname, encoding="utf-8") as FileData:
+            self.textbox.setText(FileData.read())
+
+
+    def closeEvent(self, event):
+        reply = QMessageBox.question(self, 'Message', "Are you sure to quit?",
+                                QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+        if reply == QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+if __name__ == "__main__":
+
+    ex = BrailleGui()
+    sys.exit(app.exec_())
